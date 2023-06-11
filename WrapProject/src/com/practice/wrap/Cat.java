835be1fd0cbd27081a351c5e368c29e7fae919c6
Wei <aszx70293@gmail.com>
2023-06-11 21:55:39 +0800

    20230611 - 3

---
+++ WrapProject/src/com/practice/wrap/Cat.java
@@ -0,0 +1,8 @@
+package com.practice.wrap;
+
+public class Cat {
+	// 成員屬性
+	String name;
+	Integer month;
+	Double weight;
+}
---
+++ WrapProject/src/com/practice/wrap/CatTest.java
@@ -0,0 +1,15 @@
+package com.practice.wrap;
+
+public class CatTest {
+
+	public static void main(String[] args) {
+		// 實例化對象
+		Cat one = new Cat();
+		
+		// 測試輸出
+		System.out.println("小貓的暱稱："+one.name);
+		System.out.println("小貓的年齡："+one.month);
+		System.out.println("小貓的體重："+one.weight);
+	}
+
+}
